5b66e9230317773f4880caf175f545d7d348613e
josemigueldelcastillo <n00272566@upn.pe>
2025-05-23 21:34:21 -0500

    Subiendo avanze frontend Caycho Pollo

---
+++ app/src/main/java/com/example/t2/vistas/regdocentes.java
@@ -0,0 +1,26 @@
+package com.example.t2.vistas;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.t2.R;
+
+public class regdocentes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_regdocentes);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
